aaebc32f6baac6abedda5a48dd0c475aa77623bd
MarkZohar <mark.zohar@student.um.si>
2017-11-02 21:39:11 +0100

    Data binding, triggers, serializacija

---
+++ Aplikacija/Aplikacija/Igra.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Windows.Media.Imaging;
+
+namespace Aplikacija
+{
+    public class Igra : INotifyPropertyChanged
+    {
+        private string naziv;
+        private string naziv_kratko;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Naziv
+        {
+            get { return naziv; }
+            set
+            {
+                naziv = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(""));
+                }
+            }
+        }
+        public string Naziv_kratko
+        {
+            get { return naziv_kratko; }
+            set
+            {
+                naziv_kratko = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(""));
+                }
+            }
+        }
+
+        public string Format
+        {
+            get { return this.Naziv; }
+        }
+
+        public Igra()
+        {
+            naziv = "/";
+        }
+    }
+}
--- Aplikacija/Aplikacija/MainWindow.xaml.cs
+++ Aplikacija/Aplikacija/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
+using System.Windows.Threading;
+using System.Collections.ObjectModel;
+using System.IO;
 
 namespace Aplikacija
 {
@@ -20,12 +24,27 @@ namespace Aplikacija
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Igra> seznam_igre = new ObservableCollection<Igra>();
 
         public MainWindow()
         {
             InitializeComponent();
 
 
+            try
+            {
+
+                XmlSerializer deserializer = new XmlSerializer(typeof(ObservableCollection<Igra>));
+                TextReader tr = new StreamReader(@"IgreDefault.xml");
+                seznam_igre.Clear();
+                seznam_igre = (ObservableCollection<Igra>)deserializer.Deserialize(tr);
+                tr.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Napaka! Ne morem prebrati iger: " + ex.Message);
+            }
 
             //List<MenuVrstica> menu = new List<MenuVrstica>();
             //menu.Add(new MenuVrstica() { Ime = "IGRE", Ikona = new BitmapImage(new Uri(@"/slike/1f608.png", UriKind.Relative))  });
@@ -33,15 +52,25 @@ namespace Aplikacija
             //menu.Add(new MenuVrstica() { Ime = "NASTAVITVE", Ikona = new BitmapImage(new Uri(@"/slike/1f637.png", UriKind.Relative)) });
 
             //tabCont.ItemsSource = menu;
+            listview_seznam_igre.ItemsSource = seznam_igre;
 
-            
-            
+
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Shrani_Click(object sender, RoutedEventArgs e)
         {
+            XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Igra>));
+            using (TextWriter writer = new StreamWriter(@"IgreDefault.xml"))
+            {
+                serializer.Serialize(writer, seznam_igre);
+                writer.Close();
+            }
+        }
 
+        private void listviewitem_seznam_igre_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Gumb dela");
         }
     }
 }
